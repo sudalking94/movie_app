5be51e27d9569f9bf37a17baa46fb42f743fde45
sudalking94 <67311672+sudalking94@users.noreply.github.com>
2021-03-05 17:04:18 +0900

    2021-03-05-17-04

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({ fav }) {
+  console.log(fav);
+  return <h3>I like {fav}</h3>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!!</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="steak" />
+      <Food fav="milk" />
+      <div></div>
     </div>
   );
 }
